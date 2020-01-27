334a0c23ee2b04327c983b0d8f6280330204a6de
dsohenrique <33877797+dsohenrique@users.noreply.github.com>
2020-01-27 12:12:47 -0300

    Initial Commit.

---
+++ view/index.js
@@ -0,0 +1,17 @@
+function sortear(){
+  let numeros = []
+  const selector = (element) => document.querySelector(element)
+  const quantidadeNumeros = selector('#qtd-numbers').value
+  const inicial = selector('#initial-value').value
+  const final = selector('#final-value').value
+  const inputSortedNumber = selector('#sorted-number')
+  
+   while (numeros.length <= quantidadeNumeros) {
+        let aleatorio = Math.floor(Math.random() * final + inicial)
+
+        if (numeros.indexOf(aleatorio) == -1)
+            aleatorio == 0 && inicial != 0 ? numeros.push(aleatorio + 1) : numeros.push(aleatorio)
+    }
+  
+  inputSortedNumber.value = numeros.sort()
+}
